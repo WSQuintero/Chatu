ca63fb1221f5c5ddd98d356afc42a397c5e5d852
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2024-01-01 20:23:24 -0500

    desplegando

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,8 +1,8 @@
-import io from 'socket.io-client';
+import io from 'socket.io-client'
 export const socket = io(
-	// 'https://chatu-backend-dev-aktd.1.us-1.fl0.io',
-	'http://localhost:3001',
-	{
-		withCredentials: true
-	}
-);
+  'https://chatu-backend-dev-aktd.1.us-1.fl0.io',
+  // 'http://localhost:3001',
+  {
+    withCredentials: true
+  }
+)
