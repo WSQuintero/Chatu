276ad126eaf67ec416f05d413e0bc1972103c60a
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2024-01-01 23:11:55 -0500

    corregido tamaño de contenedor de mensajes

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -25,7 +25,7 @@ function Chat() {
 
   return (
     <div className='bg-gradient-to-r from-white to-green-500 w-full h-[100vh] relative flex flex-col justify-center items-center'>
-      <div className='flex flex-col w-[99%] pl-3 gap-3 h-[99%] mt-20  bg-white  lg:rounded-tl-[100px] rounded-3xl overflow-auto  lg:rounded-br-[100px]  shadow-green-950 shadow-xl  justify-end text-[#37E23B]'>
+      <div className='flex flex-col w-full pl-3 gap-3 h-[98vh]  bg-white  lg:rounded-tl-[100px] rounded-3xl overflow-auto  lg:rounded-br-[100px]  shadow-green-950 shadow-xl  justify-end text-[#37E23B]'>
         <ChatMessages />
         <SendMessage />
       </div>
